9f392b77f68441b6760b94a8530eeca50b19c6b5
KashishJindal20 <156069966+KashishJindal20@users.noreply.github.com>
2024-12-04 18:43:00 +0530

    1st Commit

---
+++ _01Program.java
@@ -0,0 +1,13 @@
+//Traversing an Array
+package Basics;
+ public class  _01Program{
+    public static void main(String[]args){
+        int[] arr ={10,20,30,40,50};
+        //traverse each element of an aaray
+        for(int i =0;i<arr.length;i++){
+            System.out.println("Element at index "+ i+" is "+arr[i]);
+            
+        }
+    }
+ }
+ 
